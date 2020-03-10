4fc927931e92aecf24939c74f3d751522bb9873e
cread91 <c.read91@gmail.com>
2020-03-09 21:39:15 -0700

    button press commands added for all except operators

--- classy_calculator/calculator.cpp
+++ classy_calculator/calculator.cpp
@@ -31,3 +31,152 @@ void Calculator::foo()
 }
 
 
+
+void Calculator::on_one_button_clicked()
+{
+    if(digits <= 20)
+    {
+        display_num.append("1");
+        digits += 1;
+        ui->calc_input->setText(display_num);
+    }
+}
+
+void Calculator::on_two_button_clicked()
+{
+    if(digits <= 20)
+    {
+        display_num.append("2");
+        digits += 1;
+        ui->calc_input->setText(display_num);
+    }
+}
+
+void Calculator::on_three_button_clicked()
+{
+    if(digits <= 20)
+    {
+        display_num.append("3");
+        digits += 1;
+        ui->calc_input->setText(display_num);
+    }
+}
+
+void Calculator::on_four_button_clicked()
+{
+    if(digits <= 20)
+    {
+        display_num.append("4");
+        digits += 1;
+        ui->calc_input->setText(display_num);
+    }
+}
+
+void Calculator::on_five_button_clicked()
+{
+    if(digits <= 20)
+    {
+        display_num.append("5");
+        digits += 1;
+        ui->calc_input->setText(display_num);
+    }
+}
+
+void Calculator::on_six_button_clicked()
+{
+    if(digits <= 20)
+    {
+        display_num.append("6");
+        digits += 1;
+        ui->calc_input->setText(display_num);
+    }
+}
+
+void Calculator::on_seven_button_clicked()
+{
+    if(digits <= 20)
+    {
+        display_num.append("7");
+        digits += 1;
+        ui->calc_input->setText(display_num);
+    }
+}
+
+void Calculator::on_eight_button_clicked()
+{
+    if(digits <= 20)
+    {
+        display_num.append("8");
+        digits += 1;
+        ui->calc_input->setText(display_num);
+    }
+}
+
+void Calculator::on_nine_button_clicked()
+{
+    if(digits <= 20)
+    {
+        display_num.append("9");
+        digits += 1;
+        ui->calc_input->setText(display_num);
+    }
+}
+
+void Calculator::on_zero_button_clicked()
+{
+    if(digits <= 20)
+    {
+        display_num.append("0");
+        digits += 1;
+        ui->calc_input->setText(display_num);
+    }
+}
+
+void Calculator::on_decimal_button_clicked()
+{
+    bool decimal = 0;
+    for(int i = 0; i < digits; i++)
+    {
+        if(display_num.at(i) == ".")
+        {
+            decimal = 1;
+        }
+    }
+    if(decimal == 0 && digits <= 20)
+    {
+        display_num.append(".");
+        digits += 1;
+        ui->calc_input->setText(display_num);
+    }
+}
+
+void Calculator::on_changesign_button_clicked()
+{
+    if(display_num.at(0) == " ")
+    {
+        display_num.replace(0,1,"-");
+        sign = -1;
+        ui->calc_input->setText(display_num);
+    }
+    else
+    {
+        display_num.replace(0,1," ");
+        sign = 1;
+        ui->calc_input->setText(display_num);
+    }
+}
+
+void Calculator::on_clear_button_clicked()
+{
+    display_num = " ";
+    digits = 0;
+    sign = 1;
+    ui->calc_input->setText(display_num);
+}
+
+void Calculator::on_delete_button_clicked()
+{
+    display_num.remove(digits,1);
+    digits -= 1;
+    ui->calc_input->setText(display_num);
+}
--- classy_calculator/calculator.h
+++ classy_calculator/calculator.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class Calculator; }
 QT_END_NAMESPACE
@@ -15,6 +16,35 @@ public:
     Calculator(QWidget *parent = nullptr);
     ~Calculator();
 
+private slots:
+    void on_one_button_clicked();
+
+    void on_two_button_clicked();
+
+    void on_three_button_clicked();
+
+    void on_four_button_clicked();
+
+    void on_five_button_clicked();
+
+    void on_six_button_clicked();
+
+    void on_seven_button_clicked();
+
+    void on_eight_button_clicked();
+
+    void on_nine_button_clicked();
+
+    void on_zero_button_clicked();
+
+    void on_decimal_button_clicked();
+
+    void on_changesign_button_clicked();
+
+    void on_clear_button_clicked();
+
+    void on_delete_button_clicked();
+
 private:
     Ui::Calculator *ui;
     void foobar();
@@ -22,6 +52,11 @@ private:
     void foo();
 
     void testmeee();
+
+    int digits;
+    QString display_num = " ";
+    double val;
+    int sign = 1;
 
 };
 #endif // CALCULATOR_H
